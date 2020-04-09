ade3b95c1f7a80cf60589db724b48b789c8dc66a
Jeff Roman <rom@ieee.org>
2020-04-09 01:25:07 -0700

    fixed bug which was adding unprintable characters to some serial input

--- Comms/Code/Arduino Sketches/LoRa_test_Rx/LoRa_test_Rx.ino
+++ Comms/Code/Arduino Sketches/LoRa_test_Rx/LoRa_test_Rx.ino
@@ -3,7 +3,19 @@
  
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <stdlib.h>  // for strtok() function to split strings
+#include <string.h>  // for strstr() stringsearch
  
+#define DEBUG
+
+
+// choose which hardware serial port by toggling debug mode
+#ifdef DEBUG
+auto hsp = Serial; // use USB serial in debug
+#define debug_print(x) hsp.println(x); // provide method for tracing errors
+#else
+auto hsp = Serial1; // normal operation use UART
+#endif
 
  
 // for feather m0 RFM9x
@@ -39,17 +51,17 @@ void setup()
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
  
-  Serial.begin(9600);
+  hsp.begin(9600);
 
 
-  while (!Serial) {
+  while (!hsp) {
     delay(1);
   }
 
 
   delay(100);
   
-  Serial.println("Feather LoRa Pinger-Backer!");
+  hsp.println("Feather LoRa Pinger-Backer!");
 
  
   // manual reset
@@ -59,18 +71,18 @@ void setup()
   delay(10);
  
   while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    hsp.println("LoRa radio init failed");
+    hsp.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
     while (1);
   }
-  Serial.println("LoRa radio init OK!");
+  hsp.println("LoRa radio init OK!");
  
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
+    hsp.println("setFrequency failed");
     while (1);
   }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  hsp.print("Set Freq to: "); hsp.println(RF95_FREQ);
  
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
  
@@ -81,10 +93,10 @@ void setup()
 
 
   RH_RF95::ModemConfig modem_config = {
-  0x78, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit... bits 7-4 govern BW, 3-1 governs C.R.
+  0x72, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit... bits 7-4 govern BW, 3-1 governs C.R.
         // LSB is header mode, leave as zero. 0x_2, 0x_4, 0x_6, 0x_8 represent C.R from 4/5->4/8. 
         // All other values of bits 3-0 reserved. Useable BW ranges from 0x5_ (31.25kHz) to 0x9_ (500kHz).
-  0x94, // Reg 0x1E: Spread=512chips/symbol, CRC=enable...each bit increase in first nibble
+  0x84, // Reg 0x1E: Spread=512chips/symbol, CRC=enable...each bit increase in first nibble
         // of the byte corresponds to doubling of spreading factor (change 0x94 to 0xC4 for max)
         // Don't change bits 3-0. 
   0x0C  // Reg 0x26: LowDataRate=On, Agc=On... only bits 3 and 2 have meaning. bit 3 = mobile node?
@@ -102,6 +114,7 @@ void setup()
 }
 
 
+
  
 void loop()
 {
@@ -114,21 +127,19 @@ void loop()
  
     if (rf95.recv(buf, &len))
     {
+      char *s; // pointer for string search
+
 
       digitalWrite(LED, HIGH);
-      RH_RF95::printBuffer("Received: ", buf, len);
-      Serial.print("Got: ");
-      Serial.println((char*)buf);
-       Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
+      //RH_RF95::printBuffer("Received: ", buf, len);
+      //hsp.print("Got: ");
+      hsp.println((char*)buf);
+      hsp.print("RSSI: ");
+      hsp.println(rf95.lastRssi(), DEC);
+      hsp.print("SNR: ");
+      hsp.println(rf95.lastSNR(), DEC);
 
-/*
-      if(buf[0:6]=="setSlow"){
-        rf95.setModemConfig(RH_RF95::Bw125Cr48Sf4096);
-        Serial.println("Spreading factor 12, CRC 4/8");
-        }
-      }
-*/
+
       // Send a reply
       uint8_t data[] = "You sent me ";
       rf95.send(data, sizeof(data));
@@ -137,13 +148,13 @@ void loop()
       rf95.send(buf, len);
       rf95.waitPacketSent();
       delay(10);
-      Serial.println("Sent a reply");
+      hsp.println("Sent a reply");
       digitalWrite(LED, LOW);
     }
     else
     {
 
-      Serial.println("Receive failed.");
+      hsp.println("Receive failed.");
 
     }
   }
--- Comms/Code/Arduino Sketches/lora_serial_terminal_test/lora_serial_terminal_test.ino
+++ Comms/Code/Arduino Sketches/lora_serial_terminal_test/lora_serial_terminal_test.ino
@@ -1,37 +1,53 @@
 
-// This sketch is a proof of concept to verify packet transmission of UART data 
+// This sketch is a proof of concept to verify packet transmission of UART data
 // from one LoRa radio to another. It pings out serial input to a radio to a pingback
 // program and reports the packet that it receives in response.
 // Currently under construction to add the ability to send command to change transmit
 // settings for datarate
- 
+
 #include <SPI.h>
 #include <RH_RF95.h>
 
- 
-// for feather m0  
+
+#define DEBUG
+
+
+// choose which hardware serial port by toggling debug mode
+#ifdef DEBUG
+auto hsp = Serial; // use USB serial in debug
+#define debug_print(x) hsp.println(x); // provide method for tracing errors
+#define debug_hold() while(!hsp){delay(1);};
+#else
+auto hsp = Serial1; // normal operation use UART
+#endif
+
+
+
+// for feather m0
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
 
- 
-/* for shield 
-#define RFM95_CS 10
-#define RFM95_RST 9
-#define RFM95_INT 7
+
+
+
+/* for shield
+  #define RFM95_CS 10
+  #define RFM95_RST 9
+  #define RFM95_INT 7
 */
 
- 
-/* Feather m0 w/wing 
-#define RFM95_RST     11   // "A"
-#define RFM95_CS      10   // "B"
-#define RFM95_INT     6    // "D"
+
+/* Feather m0 w/wing
+  #define RFM95_RST     11   // "A"
+  #define RFM95_CS      10   // "B"
+  #define RFM95_INT     6    // "D"
 */
- 
+
 
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
- 
+
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
@@ -42,187 +58,152 @@ int longestMin = 0;
 int longestSec = 0;
 int seconds;
 int minutes;
- 
-void setup() 
+
+void setup()
 {
   lastMsg = millis();
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
- 
-  Serial1.begin(9600);  // Feather M0 hardware UART tx/rx are tied to "Serial1" 
-                        // and not Serial, use as with Serial library.
-                      
- /*     
-  while (!Serial) {
-    delay(1);
-  }
- */
- 
+
+  hsp.begin(9600);  // Feather M0 hardware UART tx/rx are tied to "Serial1"
+  // and not Serial, use as with Serial library.
+
+  debug_hold();
+
   delay(100);
- 
-  //Serial.println("Feather LoRa TX Test!");
-  
-  Serial1.println("Feather LoRa TX Test!");
- 
+
+
+  hsp.println("Feather LoRa TX Test!");
+
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
- 
+
+  //----------------------RFM95 Configuration Time-----------------------------------------
+
   while (!rf95.init()) {
-    Serial1.println("LoRa radio init failed");
-    Serial1.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    hsp.println("LoRa radio init failed");
+    hsp.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
     while (1);
   }
-  Serial1.println("LoRa radio init OK!");
- 
+  hsp.println("LoRa radio init OK!");
+
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial1.println("setFrequency failed");
+    hsp.println("setFrequency failed");
     while (1);
   }
-  Serial1.print("Set Freq to: "); Serial.println(RF95_FREQ);
-  
+  hsp.print("Set Freq to: "); hsp.println(RF95_FREQ);
+
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
 
 
   RH_RF95::ModemConfig modem_config = {
-  0x78, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit... bits 7-4 govern BW, 3-1 governs C.R.
-        // LSB is header mode, leave as zero. 0x_2, 0x_4, 0x_6, 0x_8 represent C.R from 4/5->4/8. 
-        // All other values of bits 3-0 reserved. Useable BW ranges from 0x5_ (31.25kHz) to 0x9_ (500kHz).
-  0x94, // Reg 0x1E: Spread=512chips/symbol, CRC=enable...each bit increase in first nibble
-        // of the byte corresponds to doubling of spreading factor (change 0x94 to 0xC4 for max)
-        // Don't change bits 3-0. 
-  0x0C  // Reg 0x26: LowDataRate=On, Agc=On... only bits 3 and 2 have meaning. bit 3 = mobile node?
-        // bit 2 = automatic AGC?
+    0x72, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit... bits 7-4 govern BW, 3-1 governs C.R.
+    // LSB is header mode, leave as zero. 0x_2, 0x_4, 0x_6, 0x_8 represent C.R from 4/5->4/8.
+    // All other values of bits 3-0 reserved. Useable BW ranges from 0x5_ (31.25kHz) to 0x9_ (500kHz).
+    0x84, // Reg 0x1E: Spread=512chips/symbol, CRC=enable...each bit increase in first nibble
+    // of the byte corresponds to doubling of spreading factor (change 0x94 to 0xC4 for max)
+    // Don't change bits 3-0.
+    0x0C  // Reg 0x26: LowDataRate=On, Agc=On... only bits 3 and 2 have meaning. bit 3 = mobile node?
+    // bit 2 = automatic AGC?
   };
-  
+
   rf95.setModemRegisters(&modem_config);
 
 
 
   // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
 }
- 
 
-
+//------------------Serial Input Methods---------------------------------------------
 
 // set vars for serial input function
 const byte numChars = 255; //limits read to 255 bytes (full size of serial buffer
-                           // and max length of a lora payload)
-char receivedChars[numChars];
+// and max length of a lora payload)
+char payLoad[numChars];
 boolean newData = false;
+int packetSize;
+int timeSent = 0;
 
-void receiveWithStartAndEndMarkers(){
-  static boolean receiveInProgress = false;
+void receiveLine() {
+
   static byte ndx = 0;
-  char startMarker = '<';
-  char endMarker = '>';
+  char endMarker = '\n';
   char rc;
 
-  while (Serial1.available() > 0 && newData == false) {
-    rc = Serial1.read();
-    if(receiveInProgress == true){
-      if (rc != endMarker){
-        receivedChars[ndx] = rc;
-        ndx++;
-        if (ndx >= numChars) {
-          ndx = numChars -1;
-        }
-      }
-      else {
-        receivedChars[ndx] = '\0';
-        receiveInProgress = false;
-        ndx = 0;
-        newData = true;
+
+  while (hsp.available() > 0) {
+    rc = hsp.read();
+    if (rc != endMarker) {
+      payLoad[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) {
+        ndx = numChars - 1;
       }
     }
-    else if(rc == startMarker){
-      receiveInProgress = true;
+    else {
+      payLoad[ndx] = '\0';
+      packetSize = ndx + 1;
+      ndx = 0;
+      newData = true;
     }
   }
 }
 
 
-void showNewData(){
-  if(newData == true) {
-    Serial1.print(receivedChars);
+void sendLine() {
+  if (newData == true) {
+    hsp.println(payLoad);
     newData = false;
+    hsp.println("Transmitting...");             // Send a message to rf95_server
+    rf95.send((uint8_t *)payLoad,packetSize);   //send() needs uint8_t so we cast it
+    rf95.waitPacketSent();
+    timeSent = millis();
   }
 }
 
 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
-String payLoad;
-int timeSent = 0;
+//--------------------------------Mainline-----------------------------------
+
+
+
 
 void loop()
 {
-  if(Serial1.available() == 0){
+  if (hsp.available() == 0) {
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
 
-    if (rf95.waitAvailableTimeout(1000)){ 
-        // Should be a reply message for us now   
-        if (rf95.recv(buf, &len)){
-          Serial1.print((millis()-timeSent)/1000.0);
-          Serial1.println(" seconds ping time.");
-          Serial1.println((char*)buf);
-          Serial1.print("RSSI: ");
-          Serial1.println(rf95.lastRssi(), DEC);
-        }
-        
-        else{
-          Serial1.println("Receive failed.");
-          timeSent = millis();
-        }
-        
+    if (rf95.waitAvailableTimeout(1000)) {
+      // Should be a reply message for us now
+      if (rf95.recv(buf, &len)) {
+        hsp.print((millis() - timeSent) / 1000.0);
+        hsp.println(" seconds ping time.");
+        hsp.println((char*)buf);
+        hsp.print("RSSI: ");
+        hsp.println(rf95.lastRssi(), DEC);
+        hsp.print("SNR: ");
+        hsp.println(rf95.lastSNR(), DEC);
       }
+
+      else {
+        hsp.println("Receive failed.");
+        timeSent = millis();
+      }
+
+    }
   }
-  
-  if(Serial1.available() > 0) {
-    //receiveWithStartAndEndMarkers();
-    //showNewData();
-    //payLoad = receivedChars;
-    payLoad = Serial1.readString();
 
-    Serial1.print("Received ");
-    Serial1.print(payLoad);
-/*    if (payLoad == "setSlow"){
-      rf95.setModemConfig(RH_RF95::Bw125Cr48Sf4096);
-      Serial1.println("Spreading factor 12, CRC 4/8");
-    }
- */
-    Serial1.println("Transmitting..."); // Send a message to rf95_server
-  
-    // make packet from float or string payload
-    uint8_t packet[(payLoad.length() + 2)];
-    for(int i = 0; i < payLoad.length(); i++){
-      packet[i]=payLoad.charAt(i);
-    }
-    
-    //char radiopacket[20] = "Hello World #      ";
-    //itoa(packetnum++, radiopacket+13, 10);
-   
-    Serial1.print("Sending "); 
-    Serial1.print(payLoad);
-    packet[payLoad.length()+1] = 0;
-    
-
-    rf95.send(packet, sizeof(packet));
-   
-    Serial.println("Waiting for packet to complete..."); 
-
-    rf95.waitPacketSent();
-    timeSent = millis();
-    Serial1.println("Waiting for reply...");
-   
-
+  if (hsp.available() > 0) {
+    receiveLine();
+    sendLine();
   }
 
 }
