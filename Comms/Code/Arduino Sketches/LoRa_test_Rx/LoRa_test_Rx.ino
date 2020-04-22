3f8151cc0ba826b2d29db286e30a469accd4bef8
Jeff Roman <rom@ieee.org>
2020-04-22 16:35:40 -0700

    LoRa settings toggle code added and tested.

--- Comms/Code/Arduino Sketches/LoRa_test_Rx/LoRa_test_Rx.ino
+++ Comms/Code/Arduino Sketches/LoRa_test_Rx/LoRa_test_Rx.ino
@@ -1,17 +1,18 @@
 // This sketch is a pingback program to respond to a ping from the LoRa radio
 // It sends back the exact message that it receives.
- 
+
 #include <SPI.h>
 #include <RH_RF95.h>
 #include <RHReliableDatagram.h>
-#include <stdlib.h>  // for strtok() function to split strings
+#include <avr/dtostrf.h>
 #include <string.h>  // for strstr() stringsearch
- 
+
 #define DEBUG
 
 
 // choose which hardware serial port by toggling debug mode
 #ifdef DEBUG
+bool db = true;
 auto hsp = Serial; // use USB serial in debug
 #define debug_print(x) hsp.println(x); // provide method for tracing errors
 #define debug_hold() while(!hsp){delay(1);};
@@ -19,39 +20,73 @@ auto hsp = Serial; // use USB serial in debug
 auto hsp = Serial1; // normal operation use UART
 #endif
 
- 
+
 // for feather m0 RFM9x
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
 
- 
-/* Feather m0 w/wing 
-#define RFM95_RST     11   // "A"
-#define RFM95_CS      10   // "B"
-#define RFM95_INT     6    // "D"
-*/
- 
 
- 
+
+
 //--------------------------LORA SETTINGS DEFS---------------------------------------------
 
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
-#define TIMEOUT 4000
+#define TIMEOUT 6000
 #define RETRIES 5
 #define SERVER_ADDRESS 1            // AUV radio is the server
 #define CLIENT_ADDRESS 2            // shore pc is the client
 
+const byte bw125cr45 = 0x72;
+const byte bw125cr46 = 0x74;
+const byte bw125cr47 = 0x76;
+const byte bw125cr48 = 0x78;
+const byte bw250cr45 = 0x82;
+const byte bw250cr46 = 0x84;
+const byte bw250cr47 = 0x86;
+const byte bw250cr48 = 0x88;
+const byte bw500cr45 = 0x92;
+const byte bw500cr46 = 0x94;
+const byte bw500cr47 = 0x96;
+const byte bw500cr48 = 0x98;
+
+const byte sf128 = 0x74;
+const byte sf256 = 0x84;
+const byte sf512 = 0x94;
+const byte sf1024 = 0xA4;
+const byte sf2048 = 0xB4;
+const byte sf4096 = 0xC4;
+
+
+typedef struct {
+  char *key;
+  byte val;
+} t_symstruct;   // create struct for setting LUTs
+
+static t_symstruct bwLUT[] = {
+  {"bw125cr45\0", bw125cr45}, {"bw250cr45\0", bw250cr45}, {"bw500cr45\0", bw500cr45},
+  {"bw125cr46\0", bw125cr46}, {"bw250cr46\0", bw250cr46}, {"bw500cr46\0", bw500cr46},
+  {"bw125cr47\0", bw125cr47}, {"bw250cr47\0", bw250cr47}, {"bw500cr47\0", bw500cr47},
+  {"bw125cr48\0", bw125cr48}, {"bw250cr48\0", bw250cr48}, {"bw500cr48\0", bw500cr48}
+};
+
+static t_symstruct sfLUT[] = {
+  {"sf128\0", sf128}, {"sf256\0", sf256}, {"sf512\0", sf512},
+  {"sf1024\0", sf1024}, {"sf2048\0", sf2048}, {"sf4096\0", sf4096}
+};
+
+
+#define N_bwKeys 12
+#define N_sfKeys 6
+
+
 // Singleton instance of the radio driver
 RH_RF95 driver(RFM95_CS, RFM95_INT);                // driver is radio hardware driver
-RHReliableDatagram rf95(driver,SERVER_ADDRESS);     // rf95 is the msg manager
+RHReliableDatagram rf95(driver, SERVER_ADDRESS);    // rf95 is the msg manager
 
- 
 // Blinky on receipt
 #define LED 13
-
-
 
 void setup()
 {
@@ -59,54 +94,42 @@ void setup()
   pinMode(LED, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
- 
+
   hsp.begin(9600);
   debug_hold();
   delay(100);
-  
+
   hsp.println("Feather LoRa Pinger-Backer!");
 
- 
+
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
- 
+
   while (!rf95.init()) {
     hsp.println("LoRa radio init failed");
     hsp.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
     while (1);
   }
   hsp.println("LoRa radio init OK!");
- 
+
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!driver.setFrequency(RF95_FREQ)) {
     hsp.println("setFrequency failed");
     while (1);
   }
   hsp.print("Set Freq to: "); hsp.println(RF95_FREQ);
- 
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-  // Change this to taste by editing modem_config below:
-
-
 
   RH_RF95::ModemConfig modem_config = {
-  0x72, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit... bits 7-4 govern BW, 3-1 governs C.R.
-        // LSB is header mode, leave as zero. 0x_2, 0x_4, 0x_6, 0x_8 represent C.R from 4/5->4/8. 
-        // All other values of bits 3-0 reserved. Useable BW ranges from 0x5_ (31.25kHz) to 0x9_ (500kHz).
-  0x84, // Reg 0x1E: Spread=512chips/symbol, CRC=enable...each bit increase in first nibble
-        // of the byte corresponds to doubling of spreading factor (change 0x94 to 0xC4 for max)
-        // Don't change bits 3-0. 
-  0x0C  // Reg 0x26: LowDataRate=On, Agc=On... only bits 3 and 2 have meaning. bit 3 = mobile node?
-        // bit 2 = automatic AGC?
+    bw125cr45,
+    sf128,
+    0x0C  // LEAVE THIS ALONE
   };
 
-  
-  //rf95.setModemConfig(RH_RF95::Bw125Cr48Sf4096);
   driver.setModemRegisters(&modem_config);
-  
+
   //set to maximum possible output power
   driver.setTxPower(23, false);
 
@@ -114,65 +137,183 @@ void setup()
   rf95.setTimeout(TIMEOUT);
 }
 
-void transmit(char msg[], int msgLength){
-  if (!rf95.sendtoWait((uint8_t *)msg, msgLength + 1, 1)){
+
+
+
+byte set[3];
+const byte numChars = 255;
+const char MODE[] = "-changemode\0";
+
+void transmit(byte msg[], int msgLength) {
+  if (!rf95.sendtoWait((uint8_t *)msg, msgLength + 1, CLIENT_ADDRESS)) {
     hsp.println("Transmission failed. No ACK received.");
   }
-  else{
-    hsp.println("Transmission successful.");
+  else {
+    hsp.println("Transmission successful. Bytes sent: ");
+    for (int i = 0; i < msgLength; i++) {
+      hsp.print(msg[i], HEX);
+    }
+    hsp.print("\n");
   }
 }
 
- 
+int keyfromstring(char *key, int param)  // param = 0 for bw/cr, 1 for sf
+{
+  debug_print("the key i'm comparing is: ");
+  debug_print(key)
+  int NKEYS;
+  if (param == 2) {
+    NKEYS = N_sfKeys;
+    for (int i = 0; i < NKEYS; i++) {
+      if (strcmp(sfLUT[i].key, key) == 0) {
+        debug_print("matched.")
+        return sfLUT[i].val;
+      }
+    }
+  }
+  else {
+    NKEYS = N_bwKeys;
+    for (int i = 0; i < NKEYS; i++) {
+      if (strcmp(bwLUT[i].key, key) == 0) {
+        debug_print("matched.")
+        return bwLUT[i].val;
+      }
+    }
+  }
+  return -1;
+}
+
+
+
+int modeChange(byte msg[]) {
+
+  set[2] = 0;
+  int sC = 0;
+  hsp.print("The char array looks like: ");
+  debug_print((char*)msg);
+  char msgCopy[numChars];
+  char newBWCR[] = "";
+  char newSF[] = "";
+  strcpy(msgCopy, (char*)msg);
+  char* rest = msgCopy;         // pointer for tokenizing
+  char* token = strtok(rest, " ");
+  while (token != NULL) {
+    switch (keyfromstring(token, 1)) {  // param 0 corresponds to bw,cr parameter
+      case -1: break;
+      case bw125cr45: set[0] = bw125cr45; strcat(newBWCR, "125kHz, 4_5, "); sC++; break;
+      case bw125cr46: set[0] = bw125cr46; strcat(newBWCR, "125kHz, 4_6, "); sC++; break;
+      case bw125cr47: set[0] = bw125cr47; strcat(newBWCR, "125kHz, 4_7, "); sC++; break;
+      case bw125cr48: set[0] = bw125cr48; strcat(newBWCR, "125kHz, 4_8, "); sC++; break;
+      case bw250cr45: set[0] = bw250cr45; strcat(newBWCR, "250kHz, 4_5, "); sC++; break;
+      case bw250cr46: set[0] = bw250cr46; strcat(newBWCR, "250kHz, 4_6, "); sC++; break;
+      case bw250cr47: set[0] = bw250cr47; strcat(newBWCR, "250kHz, 4_7, "); sC++; break;
+      case bw250cr48: set[0] = bw250cr48; strcat(newBWCR, "250kHz, 4_8, "); sC++; break;
+      case bw500cr45: set[0] = bw500cr45; strcat(newBWCR, "500kHz, 4_5, "); sC++; break;
+      case bw500cr46: set[0] = bw500cr46; strcat(newBWCR, "500kHz, 4_6, "); sC++; break;
+      case bw500cr47: set[0] = bw500cr47; strcat(newBWCR, "500kHz, 4_7, "); sC++; break;
+      case bw500cr48: set[0] = bw500cr48; strcat(newBWCR, "500kHz, 4_8, "); sC++; break;
+    }
+    switch (keyfromstring(token, 2)) {
+      case -1: break;
+      case sf128: set[1] = sf128; strcat(newSF, "128\0"); sC++; break;
+      case sf256: set[1] = sf256; strcat(newSF, "256\0"); sC++; break;
+      case sf512: set[1] = sf512; strcat(newSF, "512\0"); sC++; break;
+      case sf1024: set[1] = sf1024; strcat(newSF, "1024\0"); sC++; break;
+      case sf2048: set[1] = sf2048; strcat(newSF, "2048\0"); sC++; break;
+      case sf4096: set[1] = sf4096; strcat(newSF, "4096\0"); sC++; break;
+    }
+    token = strtok(NULL, " ");
+  }
+
+  int c = 0;
+  debug_print("Set[0-2] =");
+  while (c < 3) {
+    debug_print(set[c]);
+    c++;
+  }
+  if (sC < 2) {
+    hsp.println("That didn't work.");
+    hsp.println("Try again with the settings format -changemode bw___cr__ sf___");
+    return -1;
+  }
+  char confirmation[numChars] = "";
+  strcat(confirmation, "-received- New settings: ");
+  strcat(confirmation, newBWCR);
+  strcat(confirmation, newSF);
+  debug_print(newBWCR);
+  debug_print(newSF);
+  debug_print(confirmation);
+  delay(100);
+  if (!rf95.sendtoWait((uint8_t *)confirmation, strlen(confirmation) + 1, CLIENT_ADDRESS)) {
+    hsp.println("No Ack received, cannot change mode.");
+    return -1;
+  }
+  else {
+    hsp.println("Acknowledge received. Changing mode now.");
+
+    RH_RF95::ModemConfig new_modem_config = {
+      set[0],
+      set[1],
+      0x0C  // LEAVE THIS ALONE
+    };
+    delay(10);
+    driver.setModemRegisters(&new_modem_config);      // give it the configuration we specified
+    delay(10);
+    if(db){driver.printRegisters();}
+
+  }
+  return 0;
+}
+
+
+int i = 0;
+
 void loop()
 {
-  
+
   if (rf95.available())
   {
     // Should be a message for us now
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t buf[numChars];
     uint8_t len = sizeof(buf);
     uint8_t from;
-    if(rf95.recvfromAck(buf, &len, &from)){
-        hsp.print("Message from client: "); hsp.println(from);
-        hsp.println((char*)buf);
-        
-    }
-    else{
-      hsp.println("Receive failed.");
-    }
+    if (rf95.recvfromAck(buf, &len, &from)) {
+      i++;
+      if (i == 2) {
+        hsp.println("Changing settings now.");
+        RH_RF95::ModemConfig new_modem_config = {
+          150,
+          164,
+          0x0C  // LEAVE THIS ALONE
+        };
+        delay(10);
+        driver.setModemRegisters(&new_modem_config);      // give it the configuration we specified
+        delay(10);
+        if(db){driver.printRegisters();}
+      }
       
-    /*
-    if (rf95.recv(buf, &len))
-    {
-      char *s; // pointer for string search
-
-
-      digitalWrite(LED, HIGH);
-      //RH_RF95::printBuffer("Received: ", buf, len);
-      //hsp.print("Got: ");
+      hsp.print("Message from client: "); hsp.println(from);
       hsp.println((char*)buf);
-      hsp.print("RSSI: ");
-      hsp.println(rf95.lastRssi(), DEC);
-      hsp.print("SNR: ");
-      hsp.println(rf95.lastSNR(), DEC);
-
-
-      // Send a reply
-      uint8_t data[] = "You sent me ";
-      rf95.send(data, sizeof(data));
-      rf95.waitPacketSent();
-      delay(10);
-      rf95.send(buf, len);
-      rf95.waitPacketSent();
-      delay(10);
-      hsp.println("Sent a reply");
-      digitalWrite(LED, LOW);
+      for (int i = 0; i < len; i++) {
+        hsp.print(buf[i], HEX);
+      }
+      hsp.print("\n");
+      char tmp[numChars];
+      for (int i = 0; i < numChars; i++) {
+        tmp[i] = (char)buf[i];
+      }
+      char *token = strtok(tmp, " ");
+      while ((token != NULL) && (token != "\n")) {
+        if (strcmp(token, MODE) == 0) {
+          debug_print("Request to change settings.");
+          if (modeChange(buf) == 0) {
+            hsp.println("Transmission settings changed successfully.");
+          }
+        }
+        token = strtok(NULL, " ");
+      }
     }
-    else
-    {
-      hsp.println("Receive failed.");
-    }
-    */
+
   }
+
 }
--- Comms/Code/Arduino Sketches/lora_serial_terminal_test/lora_serial_terminal_test.ino
+++ Comms/Code/Arduino Sketches/lora_serial_terminal_test/lora_serial_terminal_test.ino
@@ -9,6 +9,7 @@
 #include <RH_RF95.h>
 #include <avr/dtostrf.h>
 #include <RHReliableDatagram.h>
+#include <string.h>
 
 
 //------------------------DEBUG DEFINITIONS---------------------------------------------
@@ -18,6 +19,7 @@
 
 // choose which hardware serial port by toggling debug mode
 #ifdef DEBUG
+bool db = true;
 auto hsp = Serial; // use USB serial in debug
 #define debug_print(x) hsp.println(x); // provide method for tracing errors
 #define debug_hold() while(!hsp){delay(1);};
@@ -39,45 +41,69 @@ auto hsp = Serial1; // normal operation use UART
 #define RFM95_RST 4
 #define RFM95_INT 3
 
-/* for shield
-  #define RFM95_CS 10
-  #define RFM95_RST 9
-  #define RFM95_INT 7
-*/
-
-/* Feather m0 w/wing
-  #define RFM95_RST     11   // "A"
-  #define RFM95_CS      10   // "B"
-  #define RFM95_INT     6    // "D"
-*/
 
 //--------------------------LORA SETTINGS DEFS---------------------------------------------
 
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
-#define TIMEOUT 4000
+#define TIMEOUT 6000
 #define RETRIES 5
 #define SERVER_ADDRESS 1            // AUV radio is the server
 #define CLIENT_ADDRESS 2            // shore pc is the client
 
+// SETTING OPTIONS FOR REGISTER 1D
+
+const byte bw125cr45 = 0x72;
+const byte bw125cr46 = 0x74;
+const byte bw125cr47 = 0x76;
+const byte bw125cr48 = 0x78;
+const byte bw250cr45 = 0x82;
+const byte bw250cr46 = 0x84;
+const byte bw250cr47 = 0x86;
+const byte bw250cr48 = 0x88;
+const byte bw500cr45 = 0x92;
+const byte bw500cr46 = 0x94;
+const byte bw500cr47 = 0x96;
+const byte bw500cr48 = 0x98;
+
+
+// SETTING OPTIONS FOR REGISTER 1E
+const byte sf128 = 0x74;
+const byte sf256 = 0x84;
+const byte sf512 = 0x94;
+const byte sf1024 = 0xA4;
+const byte sf2048 = 0xB4;
+const byte sf4096 = 0xC4;
+
+typedef struct {
+  char *key;
+  byte val;
+} t_symstruct;   // create struct for setting LUTs
+
+static t_symstruct bwLUT[] = {
+  {"bw125cr45\0", bw125cr45}, {"bw250cr45\0", bw250cr45}, {"bw500cr45\0", bw500cr45},
+  {"bw125cr46\0", bw125cr46}, {"bw250cr46\0", bw250cr46}, {"bw500cr46\0", bw500cr46},
+  {"bw125cr47\0", bw125cr47}, {"bw250cr47\0", bw250cr47}, {"bw500cr47\0", bw500cr47},
+  {"bw125cr48\0", bw125cr48}, {"bw250cr48\0", bw250cr48}, {"bw500cr48\0", bw500cr48}
+};
+
+static t_symstruct sfLUT[] = {
+  {"sf128\0", sf128}, {"sf256\0", sf256}, {"sf512\0", sf512},
+  {"sf1024\0", sf1024}, {"sf2048\0", sf2048}, {"sf4096\0", sf4096}
+};
+
+#define N_bwKeys 12
+#define N_sfKeys 6
+
+
 // Singleton instance of the radio driver
 RH_RF95 driver(RFM95_CS, RFM95_INT);                // driver is radio hardware driver
-RHReliableDatagram rf95(driver,SERVER_ADDRESS);     // rf95 is the msg manager
-
-//--------------------------Ping Time Variables--------------------------------------------
+RHReliableDatagram rf95(driver, CLIENT_ADDRESS);    // rf95 is the msg manager
 
 
-unsigned long lastMsg;
-unsigned long timeSince;
-unsigned long longestBreak = 0;
-int longestMin = 0;
-int longestSec = 0;
-int seconds;
-int minutes;
 
 void setup()
 {
-  lastMsg = millis();
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
@@ -95,7 +121,7 @@ void setup()
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
 
-  //----------------------RFM95 Configuration Time-----------------------------------------
+  //-------------------------RFM95 Configuration Time-----------------------------------------
 
   while (!rf95.init()) {
     hsp.println("LoRa radio init failed");
@@ -109,21 +135,13 @@ void setup()
     hsp.println("setFrequency failed");
     while (1);
   }
-  
+
   hsp.print("Set Freq to: "); hsp.println(RF95_FREQ);
 
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-  // Change this to taste by editing modem_config below:
-
   RH_RF95::ModemConfig modem_config = {
-    0x72, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit... bits 7-4 govern BW, 3-1 governs C.R.
-    // LSB is header mode, leave as zero. 0x_2, 0x_4, 0x_6, 0x_8 represent C.R from 4/5->4/8.
-    // All other values of bits 3-0 reserved. Useable BW ranges from 0x5_ (31.25kHz) to 0x9_ (500kHz).
-    0x84, // Reg 0x1E: Spread=512chips/symbol, CRC=enable...each bit increase in first nibble
-    // of the byte corresponds to doubling of spreading factor (change 0x94 to 0xC4 for max)
-    // Don't change bits 3-0.
-    0x0C  // Reg 0x26: LowDataRate=On, Agc=On... only bits 3 and 2 have meaning. bit 3 = mobile node?
-    // bit 2 = automatic AGC?
+    bw125cr45,
+    sf128,
+    0x0C  // LEAVE THIS ALONE
   };
 
   driver.setModemRegisters(&modem_config);      // give it the configuration we specified
@@ -136,21 +154,28 @@ void setup()
 
 // set vars for serial input function
 const byte numChars = 255;  // limits read to 255 bytes (full size of serial buffer
-                            // and max length of a lora payload)
-char msgBuffer[numChars];
+// and max length of a lora payload)
+byte msgBuffer[numChars];   // Serial input buffer
 boolean newData = false;
-int packetSize;
-int timeSent = 0;
+byte set[3];      // global array to hold the settings
+
+byte buf[numChars];      // receive buffer
+byte len = sizeof(buf);
+byte from;
+
+const char MODE[] = "-changemode\0";
+const char RCVD[] = "-received-\0";
+int msgCount = 0;
 
 void receiveLine() {
   static byte ndx = 0;
-  char endMarker = '\n';
-  char rc;
+  byte endMarker = '\n';
+  byte rc;
   while (hsp.available() > 0) {
     rc = hsp.read();
 
-// -----------------------NEED ERROR CORRECTION IF INPUT > 253 CHARS-------------
-    
+    // -----------------------STILL NEED ERROR CORRECTION IF INPUT > 253 CHARS-------------
+
     if ((rc != endMarker) && (ndx < (numChars - 2))) {
       msgBuffer[ndx] = rc;
       ndx++;
@@ -159,7 +184,7 @@ void receiveLine() {
       }
     }
     else {
-      msgBuffer[ndx] = '\n';
+      msgBuffer[ndx] = endMarker;
       ndx++;
       msgBuffer[ndx] = '\0';
       ndx = 0;
@@ -168,26 +193,165 @@ void receiveLine() {
   }
 }
 
-//----------------------LoRa methods---------------------------------------------
 
-void transmit(char msg[], int msgLength){
-  if (!rf95.sendtoWait((uint8_t *)msg, msgLength+1, 1)){
+int keyfromstring(char *key, int param)  // param = 0 for bw/cr, 1 for sf
+{
+  debug_print("the key i'm comparing is: ");
+  debug_print(key)
+  int NKEYS;
+  if (param == 2) {
+    NKEYS = N_sfKeys;
+    for (int i = 0; i < NKEYS; i++) {
+      if (strcmp(sfLUT[i].key, key) == 0) {
+        debug_print("matched.")
+        return sfLUT[i].val;
+      }
+    }
+  }
+  else {
+    NKEYS = N_bwKeys;
+    for (int i = 0; i < NKEYS; i++) {
+      if (strcmp(bwLUT[i].key, key) == 0) {
+        debug_print("matched.")
+        return bwLUT[i].val;
+      }
+    }
+  }
+  return -1;
+}
+
+
+//------------------------------LoRa methods---------------------------------------------
+
+void transmit(byte msg[], int msgLength) {
+  if (!rf95.sendtoWait((uint8_t *)msg, msgLength + 1, SERVER_ADDRESS)) {
     hsp.println("Transmission failed. No ACK received.");
   }
-  else{
-    hsp.println("Transmission successful.");
+
+  else {
+    hsp.println("Transmission successful. Bytes sent: ");
+
+    for (int i = 0; i <= msgLength; i++) {
+      hsp.print(msg[i], HEX);
+    }
+
+    hsp.print("\n");
+    msgCount++;
+    if (msgCount == 2) {
+      hsp.println("changing settings now.");
+      RH_RF95::ModemConfig new_modem_config = {
+        150,
+        164,
+        0x0C  // LEAVE THIS ALONE
+      };
+      delay(10);
+      driver.setModemRegisters(&new_modem_config);      // give it the configuration we specified
+      delay(10);
+      if(db){driver.printRegisters();}
+    }
+  }
+}
+
+int modeChange(byte msg[]) {
+  set[2] = 0;
+  int c = 0;
+  char msgCopy[numChars];
+  strcpy(msgCopy, (char*)msg);
+  char* rest = msgCopy;
+  char* token = strtok(rest, "\n");       // clean off newline
+  token = strtok(rest, "\r");       // clean off carriage return
+
+  while (token = strtok_r(rest, " ", &rest)) {
+    if (c == 1) {
+      switch (keyfromstring(token, c)) {  // param 0 corresponds to bw,cr parameter
+        case -1:
+          hsp.println("not a valid setting.");
+          hsp.println("Try again with the settings format -changemode bw___cr__ sf___");
+          set[0] = bw125cr45;
+          set[2] = 1;    // must check set[2] to ensure valid overall settings
+          break;
+        case bw125cr45: set[0] = bw125cr45; break;
+        case bw125cr46: set[0] = bw125cr46; break;
+        case bw125cr47: set[0] = bw125cr47; break;
+        case bw125cr48: set[0] = bw125cr48; break;
+        case bw250cr45: set[0] = bw250cr45; break;
+        case bw250cr46: set[0] = bw250cr46; break;
+        case bw250cr47: set[0] = bw250cr47; break;
+        case bw250cr48: set[0] = bw250cr48; break;
+        case bw500cr45: set[0] = bw500cr45; break;
+        case bw500cr46: set[0] = bw500cr46; break;
+        case bw500cr47: set[0] = bw500cr47; break;
+        case bw500cr48: set[0] = bw500cr48; break;
+      }
+    }
+
+    if (c == 2) {
+      switch (keyfromstring(token, c)) {
+        case -1:
+          hsp.println("not a valid setting.");
+          hsp.println("Try again with the settings format -changemode bw___cr__ sf___");
+          set[1] = sf128;
+          set[2] = 1;    // must check set[2] to ensure valid overall settings
+          break;
+        case sf128: set[1] = sf128; break;
+        case sf256: set[1] = sf256; break;
+        case sf512: set[1] = sf512; break;
+        case sf1024: set[1] = sf1024; break;
+        case sf2048: set[1] = sf2048; break;
+        case sf4096: set[1] = sf4096; break;
+      }
+    }
+
+    debug_print(token);
+    c++;
+  }
+
+  c = 0;
+  debug_print("Set[0-2] =");
+
+  while (c < 3) {
+    debug_print(set[c]);
+    c++;
+  }
+
+  if (set[2] == 1) {
+    return -1;
+  }
+
+  if (!rf95.sendtoWait(msg, strlen((char*)msg) + 1, SERVER_ADDRESS)) {
+    hsp.println("No Ack received, cannot change mode.");
+    return -1;
+  }
+
+  else {
+    hsp.println("Acknowledge received. Will change mode once server mode-change is confirmaed.");
+
+
+    return 0;
   }
 }
 
 void transmitLine() {
   if (newData == true) {
-    hsp.println(msgBuffer);
+    hsp.println((char*)msgBuffer);
     newData = false;
-    hsp.println("Transmitting...");             // Send a message to rf95_server
-    transmit(msgBuffer, strlen(msgBuffer));  //send() needs uint8_t so we cast it
-    timeSent = millis();
+    char tmp[numChars];
+    strcpy(tmp, (char*)msgBuffer);
+    char *token = strtok(tmp, " ");
+    if (strcmp(token, MODE) == 0) {
+      if (modeChange(msgBuffer) == 0) {
+        hsp.println("Transmission settings succesfully changed.");
+      }
+    }
+    else {
+      hsp.println("Transmitting...");             // Send a message to rf95_server
+      transmit(msgBuffer, strlen((char*)msgBuffer));  //send() needs uint8_t so we cast it
+    }
   }
+
 }
+
+//--------------------Battery Monitoring Functions-----------------------------
 
 void battLevel() {
   float vBatt = analogRead(VBATPIN);
@@ -199,7 +363,7 @@ void battLevel() {
   char voltagePacket[] = "";
   strcat(voltagePacket, "Current battery voltage: ");
   strcat(voltagePacket, cBuff);
-  transmit(voltagePacket, strlen(voltagePacket));
+  transmit((uint8_t*)voltagePacket, strlen(voltagePacket));
 }
 
 
@@ -211,7 +375,7 @@ void battCheck() {
   if (cmpVoltage < USB_VOLTAGE) {
     char helpMsg[numChars] = "";
     strcat(helpMsg, "The power system has failed. Please come find me. I'm scared.");
-    transmit(helpMsg, strlen(helpMsg));
+    transmit((uint8_t*)helpMsg, strlen(helpMsg));
     battLevel();
     delay(30000);       // wait 30 seconds
   }
@@ -229,36 +393,49 @@ void loop()
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
     uint8_t from;
-    
-    if (rf95.waitAvailableTimeout(1000)) {
+    char tmp[numChars];
+
+    if (rf95.available()) {
       // Should be a reply message for us now
-      if(rf95.recvfromAck(buf, &len, &from)){
+      if (rf95.recvfromAck(buf, &len, &from)) {
         hsp.print("Message from client: "); hsp.println(from);
         hsp.println((char*)buf);
-      }
-  
-      /*    
-      if (rf95.recv(buf, &len)) {
-        hsp.print((millis() - timeSent) / 1000.0);
-        hsp.println(" seconds ping time.");
-        hsp.println((char*)buf);
         hsp.print("RSSI: ");
-        hsp.println(rf95.lastRssi(), DEC);
+        hsp.println(driver.lastRssi(), DEC);
         hsp.print("SNR: ");
-        hsp.println(rf95.lastSNR(), DEC);
+        hsp.println(driver.lastSNR(), DEC);
+        for (int i = 0; i < numChars; i++) {
+          tmp[i] = (char)buf[i];
+        }
+        char *token = strtok(tmp, " ");
+        while (token != NULL) {
+          if (strcmp(token, RCVD) == 0) {
+
+            RH_RF95::ModemConfig new_modem_config = {
+              set[0],
+              set[1],
+              0x0C  // LEAVE THIS ALONE
+            };
+            delay(10);
+            driver.setModemRegisters(&new_modem_config);      // give it the configuration we specified
+            delay(10);
+            
+            if (db) {
+              driver.printRegisters();
+              }
+
+            hsp.println("confirmed. Mode has been changed.");
+          }
+          token = strtok(NULL, " ");
+        }
       }
-      else {
-        hsp.println("Receive failed.");
-        timeSent = millis();
-      }
-      */
-      
     }
   }
 
   if (hsp.available() > 0) {
     receiveLine();
     transmitLine();
+
   }
 
 }
