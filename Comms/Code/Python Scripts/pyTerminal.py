b348a976b982eff9365bd5cbe6d9cd1d2e7830de
Jeff Roman <rom@ieee.org>
2020-05-17 01:41:04 -0700

    it's a daemon now

--- Comms/Code/Python Scripts/pyTerminal.py
+++ Comms/Code/Python Scripts/pyTerminal.py
@@ -2,28 +2,27 @@ import serial
 import time
 import os
 import subprocess
-from systemd.daemon import notify, Notification
+import systemd.daemon as SD
+import Adafruit_BBIO.UART as UART
 
+ # SET THE THINGS UP
 
-
-ser = serial.Serial()
+UART.setup("UART2")
 thePort = "/dev/ttyO2"
-#thePort = "COM7"
 cwd = os.getcwd()
 outLog = cwd + "/loraIO.txt"
+
+ser = serial.Serial()
 ser.port = thePort
 ser.baudrate = 9600
 ser.bytesize = serial.EIGHTBITS #number of bits per bytes
 ser.parity = serial.PARITY_NONE #set parity check: no parity
 ser.stopbits = serial.STOPBITS_ONE #number of stop bits
-
 ser.timeout = 1            #non-block read
 ser.xonxoff = False     #disable software flow control
 ser.rtscts = False     #disable hardware (RTS/CTS) flow control
 ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
 ser.writeTimeout = 1     #timeout for write
-
-
 
 def initPort(thePort):
     try:
@@ -41,55 +40,60 @@ def getAck():
         chunk = ser.read(numBytes)
         chunk = chunk.replace('\n','').replace('\r','')
         if (str(chunk.split()[0]) != "..."):
-	    return 0
-	else:
-	    return 1
+            return 0
+        else:
+            return 1
 
 def main():
     initPort(thePort)
     if ser.is_open:
         try:
             ser.flushInput() #flush input buffer, discarding all its contents
-            ser.flushOutput() #flush output buffer, aborting current output    
-	    try:
-		os.remove(outLog)
-	    except:
-		pass
+            ser.flushOutput() #flush output buffer, aborting current output
+            SD.notify('READY=1')
+            try:
+                os.remove(outLog)
+            except:
+                pass
             while True:
                 if os.path.exists(outLog):
                     with open(outLog,"r") as f:
-    		        lines = f.readlines()
+                        lines = f.readlines()
                         for line in lines:
-		    	    if line[0:2] == '\n':
-			        pass
-			    else:
+                            if line[0:2] == '\n':
+                                pass
+                            else:
+                                SD.notify('WATCHDOG=1')
                                 ser.write(line.encode())
                                 time.sleep(0.01)
                                 ack = getAck()
-		                while(ack<1):
-			            time.sleep(0.01)
-			            ack = getAck()
-		    os.remove(outLog)
+                                c = 1000    #try each line at most ten seconds
+                                while((ack<1) and (c>0)):
+                                    time.sleep(0.01)
+                                    ack = getAck()
+                                    c = c -1
+                        os.remove(outLog)
                 numBytes = ser.in_waiting
                 if(numBytes>0):
                     chunk = ser.read(numBytes)
                     key = chunk[0].encode('ascii')
                     if (key =="-"):
-			print('not a system call.')
                         pass
                     else:
-			os.system('touch ' + outLog)
-			chunk = chunk.replace('\n','').replace('\r','')
-			with open(outLog,'w+') as outFile:
-                            subprocess.call([chunk],shell=True,stdout=outFile,stderr=outFile)
-                time.sleep(0.01)
-		notify(Notification.WATCHDOG)
-            ser.close()
+                        os.system('touch ' + outLog)
+                        chunk = chunk.replace('\n','').replace('\r','')
+                        with open(outLog,'w+') as outFile:
+                                subprocess.call([chunk],
+                                                shell=True,
+                                                stdout=outFile,
+                                                stderr=outFile)
+                        time.sleep(0.01)
+                SD.notify("WATCHDOG=1")
         except Exception as e1:
             print ("error communicating...: " + str(e1))
-    
+
     else:
         print ("cannot open serial port.")
-    
+
 if (__name__ == '__main__'):
     main()
