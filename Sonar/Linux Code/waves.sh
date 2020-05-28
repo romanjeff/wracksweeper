9734ecd921fe5c10b0964a8fe038cacba55d8249
romanminko <43760952+romanminko@users.noreply.github.com>
2020-05-27 19:29:09 -0700

    Added script to run program

---
+++ Sonar/Linux Code/waves.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# script for running the sonar software
+
+echo "You have entered the underwater search for the Spanish Galleon"
+
+cd wracksweeper
+make clean
+make
+./RecordData
+
+echo "ALL DONE"
